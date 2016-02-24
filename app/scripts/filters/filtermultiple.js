c4f55fc2e3f3135b3d5497d6cce34fe0d9ad69e8
Alvaro Uribe <alvaro.uribe@spmassets.com>
2016-02-24 17:15:57 +1300

    Working filter

--- app/scripts/filters/filtermultiple.js
+++ app/scripts/filters/filtermultiple.js
@@ -9,8 +9,59 @@
  * Filter in the angularTestApp.
  */
 angular.module('angularTestApp')
-  .filter('filterMultiple', function () {
-    return function (input) {
-      return 'filterMultiple filter: ' + input;
+  .filter('filterMultiple',['$filter',function ($filter) {
+	return function (items, keyObj) {
+		var filterObj = {
+							data:items,
+							filteredData:[],
+							applyFilter : function(obj,key){
+								var fData = [];
+								if(this.filteredData.length == 0)
+									this.filteredData = this.data;
+								if(obj){
+									var fObj = {};
+									if(angular.isString(obj)){
+										fObj[key] = obj;
+										fData = fData.concat($filter('filter')(this.filteredData,fObj));
+									}else if(angular.isArray(obj)){
+										if(obj.length > 0){	
+											for(var i=0;i<obj.length;i++){
+												if(angular.isString(obj[i])){
+													fObj[key] = obj[i];
+													fData = fData.concat($filter('filter')(this.filteredData,fObj));	
+												}
+											}
+											
+										}										
+									}									
+									if(fData.length > 0){
+										this.filteredData = fData;
+									}
+								}
+							}
+				};
+
+		if(keyObj){
+			angular.forEach(keyObj,function(obj,key){
+				filterObj.applyFilter(obj,key);
+			});			
+		}
+		
+		return filterObj.filteredData;
+	}
+}]);
+
+angular.module('angularTestApp')
+	.filter('unique', function() {
+    return function(input, key) {
+        var unique = {};
+        var uniqueList = [];
+        for(var i = 0; i < input.length; i++){
+            if(typeof unique[input[i][key]] == "undefined"){
+                unique[input[i][key]] = "";
+                uniqueList.push(input[i]);
+            }
+        }
+        return uniqueList;
     };
-  });
+});
